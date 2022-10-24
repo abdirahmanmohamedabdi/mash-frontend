637b2f8ebf264ac804d00b9f51e197edb25c9344
Bonyonte <magogobonyonte@gmail.com>
2022-10-24 19:39:54 +0300

    create variables for storing secrets

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { gapi} from "gapi-script";
 import Navigation from "./components/Navbar";
 import { AuthProvider } from "./components/AuthProvider";
 
@@ -10,6 +11,11 @@ import "./App.css";
 import { Routes, Route } from "react-router-dom";
 
 function App() {
+
+  const calenderID = process.env.REACT_APP_CALENDER_ID;
+  const apiKey = process.env.REACT_APP_GOOGLE_API_KEY;
+  const accessToken = process.env.REACT_APP_GOOGLE_ACCESS_TOKEN; 
+
   return (
     <AuthProvider>
       <div className="App">
@@ -19,6 +25,11 @@ function App() {
           <Route path="/signup" element={<SignUpForm />} />
           <Route path="/login" element={<Login />} />
         </Routes>
+      </div>
+      <div className = "App pt-4">
+      <h1 className="text-2xl font-bold mb-4">
+        React App with Google Calendar API!
+      </h1>
       </div>
     </AuthProvider>
   );
