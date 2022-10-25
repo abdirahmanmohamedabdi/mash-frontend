ee60fb2ff5e040b27c7df822d70e60725cca59d1
Abdirahman <abdirahmanabdi.ma@gmail.com>
2022-10-26 02:08:43 +0300

    changes

--- src/pages/Location.jsx
+++ src/pages/Location.jsx
@@ -2,9 +2,25 @@ import { useState, useEffect } from "react";
 import { Map, GoogleApiWrapper, Marker } from "google-maps-react";
 
 function MapCtx(props) {}
+function handleSubmit(e){
+    e.preventDefault()
 
+    fetch('/api/location', {
+        method:"POST",
+        headers: {
+            'contentType': 'application/json'
+        },
+        body: JSON.stringify()
+    })
+    .then(res => res.json())
+    
+}
 function Location(props) {
   const [position, setPosition] = useState({});
+  const state = {
+    status: false,
+    switchButton: "Off"
+}
   const [fetched, setFetched] = useState(false);
   useEffect(() => {
     if (navigator.geolocation) {
@@ -15,7 +31,7 @@ function Location(props) {
     }
   }, []);
   return (
-    <div>
+    <div className="Mapstyles">
       {fetched && (
         <Map google={props.google} initialCenter={position}>
           <Marker />
