0dece2320dc48159824a5031c0c9779e68b29f38
Bonyonte <magogobonyonte@gmail.com>
2022-10-25 09:49:32 +0300

    design the calender structure

--- src/App.js
+++ src/App.js
@@ -6,15 +6,45 @@ import { AuthProvider } from "./components/AuthProvider";
 import { Home } from "./pages/Home"
 import SignUpForm from "./pages/Signup"
 import Login from "./pages/Login";
+import Event from "./components/Event";
 
 import "./App.css";
 import { Routes, Route } from "react-router-dom";
 
 function App() {
+  const [events, setEvents] = useState([]);
 
   const calenderID = process.env.REACT_APP_CALENDER_ID;
   const apiKey = process.env.REACT_APP_GOOGLE_API_KEY;
   const accessToken = process.env.REACT_APP_GOOGLE_ACCESS_TOKEN; 
+
+  const getEvents = (calenderID, apiKey) =>{
+    function initiate() {
+      gapi.client 
+      .init({
+        apiKey: apiKey,
+      })
+      .then(function (){
+        return gapi.client.request({
+          path: 'https://www.googleapis.com/calendar/v3/calendars/${calendarID}/events'
+        });
+      })
+      .then ((response) => {
+        let events = response.result.items;
+        setEvents(events) ;
+      }, 
+      function (err){
+        return [false, err];
+      }
+      );
+    }
+    gapi.load("client", initiate);
+
+  };
+  useEffect(() => {
+    const events = getEvents(calenderID, apiKey);
+    setEvents(events);
+  }, []);
 
   return (
     <AuthProvider>
@@ -26,9 +56,16 @@ function App() {
           <Route path="/login" element={<Login />} />
         </Routes>
       </div>
-      <div className = "App pt-4">
+      <div className = "App py-8 flex-col justify-center">
       <h1 className="text-2xl font-bold mb-4">
-        React App with Google Calendar API!
+        Google Calendar
+        <ul>
+          {events?.map((event) => (
+            <li key={event.id} className="flex justify-center">
+              <Event description={event.summary} />
+            </li>
+          ))}
+        </ul>
       </h1>
       </div>
     </AuthProvider>
