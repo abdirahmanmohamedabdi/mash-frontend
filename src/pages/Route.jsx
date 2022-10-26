c8e00408a119468a2b22169f91415e8e9b77ffc4
DaisyChepyegon <daisychepyegon@gmail.com>
2022-10-26 11:34:31 +0300

    fetch routes form backend

--- src/pages/Route.jsx
+++ src/pages/Route.jsx
@@ -1,9 +1,38 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
 
 function Route() {
+
+  const [route, setRoute] = useState([])
+
+  async function fetchingroutes(){
+    await fetch("http://127.0.0.1:3000/route_plans")
+    .then((resp) => resp.json())
+    .then((route) => setRoute(route));
+  }
+
+  useEffect(() => {
+    fetchingroutes()
+   
+  }, []);
+  console.log(route)
+
+  let container = route.map((item) => (
+    <div className='contains'>
+      <div className='right'>
+      <h3>route name:{item.name}</h3>
+      <h4>{item.month}</h4>
+      </div>
+     <div className='left'>
+      <button onClick={handleDelete}>remove Route</button>
+     </div>
+      
+    </div>
+
+))
+
   return (
     <div>
-      
+      {container}
     </div>
   )
 }
