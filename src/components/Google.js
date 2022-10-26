1cfb03fe419f16324c6ed902e10c06510528b1e6
Bonyonte <magogobonyonte@gmail.com>
2022-10-26 13:14:41 +0300

    display calender features

--- src/components/Google.js
+++ src/components/Google.js
@@ -14,7 +14,27 @@ export default function Google() {
                 <p>Login to Google</p>
                 <button onClick={()=>getAuthToGoogle()}>Sign In</button>
                 </div>:
-                }
+                <div className='body'>
+                    <div className='signout'>
+                        <p>Email: {googleAuthedEmail}</p>
+                        <button onClick={()=>_signOutFromGoogle()}>Sign Out</button>
+                    </div>
+                    <form>
+                        <div className='eventItem'>
+                            <label>Description</label>
+                            <input placeholder = 'Description...' value={description} onChange={e=>setDescription(e.target.value)}></input>
+                        </div>
+                        <div className='eventItem'>
+                            <label>Start Time</label>
+                            <input type="datetime-local" value={startTime} onChange={e=>setStartTime(e.target.value)}></input>
+                        </div>
+                        <div className='eventItem'>
+                            <label>End Time</label>
+                            <input type="datetime-local" value={endTime} onChange={e=>setEndTime(e.target.value)}></input>
+                        </div>
+                        <button type='submit' onClick={(e)=>submit(e)}>Submit</button>
+                    </form>
+                </div>}
         </div>
     )
 }
