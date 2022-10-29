c499609f1e388990e15a40ed54dde2f8ea3e49de
Bonyonte <magogobonyonte@gmail.com>
2022-10-30 01:37:36 +0300

    add sidebar

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
-import React, { useEffect, useState,useContext } from "react";
-import { gapi} from "gapi-script";
+import React from "react";
 import Navigation from "./components/Navbar";
 import { AuthProvider } from "./components/AuthProvider";
 import Location from "./pages/Location";
@@ -7,6 +6,9 @@ import { Home } from "./pages/Home"
 import SignUpForm from "./pages/Signup"
 import Login from "./pages/Login";
 import Google from "./components/Google";
+import About from "./pages/About";
+import Dashboard from "./pages/Dashboard"
+import Sidebar from "./components/Sidebar"
 
 import "./App.css";
 import { Routes, Route,Navigate } from "react-router-dom";
@@ -14,70 +16,15 @@ import Manager from "./pages/Manager";
 
 
 function App() {
-  const [events, setEvents] = useState([]);
-  // const calendarID = process.env.REACT_APP_CALENDER_ID;
-  // const apiKey = process.env.REACT_APP_GOOGLE_API_KEY;
-  // const accessToken = process.env.REACT_APP_GOOGLE_ACCESS_TOKEN; 
-
-  // const getEvents = (calendarID, apiKey) =>{
-  //   function initiate() {
-  //     gapi.client 
-  //     .init({
-  //       apiKey: apiKey,
-  //     })
-  //     .then(function (){
-  //       return gapi.client.request({
-  //         path: `https://www.googleapis.com/calendar/v3/calendars/${calendarID}/events`
-  //       });
-  //     })
-  //     .then ((response) => {
-  //       let events = response.result.items;
-  //       setEvents(events) ;
-  //     }, 
-  //     function (err){
-  //       return [false, err];
-  //     }
-  //     );
-  //   }
-  //   gapi.load("client", initiate);
-
-  // };
-  // useEffect(() => {
-  //   const events = getEvents(calendarID, apiKey);
-  //   setEvents(events);
-  // }, []);
-
-  // const addEvent = (calendarID, event) => {
-  //   function initiate() {
-  //     gapi.client
-  //     .request({
-  //       path: `https://www.googleapis.com/calendar/v3/calendars/${calendarID}/events`,
-  //       method: 'POST',
-  //       body: event,
-  //       headers: {
-  //         "Content-Type": "application/json", 
-  //         Authorization: `Bearer ${accessToken}`,
-  //       }, 
-  //     })
-  //     .then(
-  //       (response) => {
-  //         return [true, response];
-  //       },
-  //       function (err) {
-  //         console.log(err);
-  //         return [false, err]; 
-  //       }
-  //     );
-  //   }
-  //   gapi.load("client", initiate);
-  // }
-
   return (
     <AuthProvider>
       <div className="App">
         <Navigation />
+        <Sidebar />
         <Routes>
-          <Route path="/" element={<Home />} /> 
+          <Route path="/" element={<Home />} />
+          <Route path="/dashboard" element={<Dashboard/>} />
+          <Route path="/about" element={<About />} />
           <Route path="/signup" element={<SignUpForm />} />
           <Route path="/location" element={<Location />}> </Route>
           <Route path="/manager" element={<Manager />}> </Route>
@@ -85,18 +32,6 @@ function App() {
         </Routes>
         <Google />
       </div>
-      {/* <div className = "App py-8 flex-col justify-center">
-      <h1 className="text-2xl font-bold mb-4">
-        Google Calendar
-        <ul>
-          {events?.map((event) => (
-            <li key={event.id} className="flex justify-center">
-              <Event description={event.summary} />
-            </li>
-          ))}
-        </ul>
-      </h1>
-      </div> */}
     </AuthProvider>
   );
 }
