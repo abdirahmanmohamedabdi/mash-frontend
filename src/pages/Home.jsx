d718cd1f16cdbb194cbda4bde12212194de40200
tangusjr <kipngenogilbert96@gmail.com>
2022-10-30 09:25:13 +0300

    Modified the page Home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import { AuthContext } from '../components/AuthProvider';
+import Merchants from '../components/Merchants'
 
 export const Home = () => {
   const { token } = React.useContext(AuthContext);
   return (
     <div>Home {token}
+    <Merchants />
     </div>
   )
 }
